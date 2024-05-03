c5dc6ab44ca5ad9393671c7d6e87cc3c95cef3e6
tkaushik015 <156272214+tkaushik015@users.noreply.github.com>
2024-05-02 23:19:44 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,39 +1,44 @@
 import streamlit as st
-st.write("1.Name: Tushar Kaushik")
 
 
 
 
-st.write("2.An explanation of how to use your webapp: what interactivity there is, what the plots/charts mean, what your conclusions were, etc: ")
+
+st.markdown("**1. Name:** Tushar Kaushik")
+
+
+
+
+st.markdown("**2. An explanation of how to use your webapp:** what interactivity there is, what the plots/charts mean, what your conclusions were, etc:")
 st.write("Ans: The web app focuses on IPL, a T20 cricket league in India, specifically analyzing data for the 2023 season. It provides insights into player performance, including the Orange Cap for most runs and the Purple Cap for most wickets. Users can select between Batting Stats and Bowling Stats, filter data by country, and visualize statistics through pie charts, box plots, tables, and correlation matrices. After selecting options and submitting, users can explore player statistics and compare multiple players using a dropdown menu.")
 
 
 
-st.write("3.Any major “gotchas” (i.e. things that don’t work, go slowly, could be improved, etc.)")
+st.markdown("**3. Any major 'gotchas' (i.e. things that don’t work, go slowly, could be improved, etc.):**")
 st.write("Ans: One potential issue is that when changing filter options after selecting players for comparison, the selected players are not automatically cleared. This functionality could be improved in future iterations.")
 
 
 
-st.write("4.What did you set out to study?  (i.e. what was the point of your project?  This should be close to your Milestone 1 assignment, but if you switched gears or changed things, note it here.)")
+st.markdown("**4. What did you set out to study? (i.e. what was the point of your project? This should be close to your Milestone 1 assignment, but if you switched gears or changed things, note it here.)**")
 st.write("Ans: Initially, the project aimed to study the correlation between player auction prices and their performance metrics. However, due to limited data availability, the focus shifted to analyzing batting and bowling statistics for the 2023 IPL season. Data from sources such as API calls and CSV files were consolidated, and insights were drawn from player performances and comparisons.")
 
 
 
-st.write("5.What did you Discover/what were your conclusions (i.e. what were your findings?  Were your original assumptions confirmed, etc.?)")
+st.markdown("**5. What did you Discover/what were your conclusions (i.e. what were your findings? Were your original assumptions confirmed, etc.?)**")
 st.write("Ans: The Streamlit app provides a user-friendly platform for cricket enthusiasts to analyze batting and bowling statistics from the 2023 IPL season. Key findings include insights into wickets distribution, batting averages, and player comparisons. Notably, the updated code ensures better readability by starting the table index from 1, enhancing the overall user experience and facilitating comprehensive cricket analysis.")
 
 
 
-st.write("6.What difficulties did you have in completing the project?")
+st.markdown("**6. What difficulties did you have in completing the project?**")
 st.write("Ans: Challenges during project completion included efficiently managing and visualizing large datasets within the Streamlit app, ensuring smooth integration of statistical analyses, and addressing user interface design considerations. Despite these hurdles, careful planning, testing, and iterative development enabled successful project completion.")
 
 
 
-st.write("7.What skills did you wish you had while you were doing the project?")
+st.markdown("**7. What skills did you wish you had while you were doing the project?**")
 st.write("Ans: Enhanced proficiency in Streamlit and advanced data visualization techniques, particularly with tools like Plotly, would have been beneficial for smoother project execution.")
 
 
 
-st.write("8.What would you do “next” to expand or augment the project?")
+st.markdown("**8. What would you do 'next' to expand or augment the project?**")
 st.write("Ans: Future project expansions could include in-depth analysis of player performance based on age, comparison of players' IPL performance over multiple seasons, and integration of international cricket performance data. Additionally, incorporating historical player records and exploring correlations between various performance metrics could provide deeper insights into player performance trends.")
 
